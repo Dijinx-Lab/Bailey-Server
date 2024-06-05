c440c9ce809eed21168f1b2e8aeea5a4738b0f09
Moasfar Javed <88271241+Moasfar-Javed@users.noreply.github.com>
2024-06-05 23:31:49 +0500

    added error logs

--- src/index.mjs
+++ src/index.mjs
@@ -18,6 +18,8 @@ const port = appConfig.server.port;
 const username = encodeURIComponent(databaseConfig.database.username);
 const password = encodeURIComponent(databaseConfig.database.password);
 
+// console.log(databaseConfig);
+
 const uri = `mongodb://${databaseConfig.database.host}:${databaseConfig.database.port}/${databaseConfig.database.dbName}`;
 MongoClient.connect(uri, {
   maxPoolSize: 50,
